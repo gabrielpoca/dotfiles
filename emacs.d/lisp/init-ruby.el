309ddfed9fb79857764d11f25177c8c8e10bc98d
Gabriel Poça <gabrielpoca@gmail.com>
2015-08-31 10:20:04 +0100

    Zip emacs folder

--- emacs.d/lisp/init-ruby.el
+++
@@ -1,7 +0,0 @@
-(dolist (exp '("Rakefile\\'" "\\.rake\\'"))
-  (add-to-list 'auto-mode-alist
-               (cons exp 'ruby-mode)))
-
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-
-(provide 'init-ruby)
