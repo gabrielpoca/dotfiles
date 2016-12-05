5061a23fd8bc9dadd26f4ce7b15552188bd88019
Gabriel Poça <gabrielpoca@gmail.com>
2016-12-05 11:57:02 +0000

    WIP pull package

--- packages/pull/lib/pull.rb
+++ packages/pull/lib/pull.rb
@@ -5,21 +5,21 @@ require "pull/version"
 module Pull
   def self.open(current_branch)
     git = Git.open(".")
-    current_branch ||= git.current_branch
+    branch = current_branch.empty? ? git.current_branch : current_branch
     remote_name = git.remote("origin").url.split(":")[1].split(".git")[0]
     github = Octokit::Client.new(netrc: true)
     organization_name = remote_name.split("/")[0]
-    pull = github.pulls(remote_name, head: "#{organization_name}:#{current_branch}").first
+    pull = github.pulls(remote_name, head: "#{organization_name}:#{branch}").first
     exec "open #{pull[:html_url]}"
   end
 
   def self.to_text(current_branch)
     git = Git.open(".")
-    current_branch ||= git.current_branch
+    branch = current_branch.empty? ? git.current_branch : current_branch
     remote_name = git.remote("origin").url.split(":")[1].split(".git")[0]
     github = Octokit::Client.new(netrc: true)
     organization_name = remote_name.split("/")[0]
-    pull = github.pulls(remote_name, head: "#{organization_name}:#{current_branch}").first
+    pull = github.pulls(remote_name, head: "#{organization_name}:#{branch}").first
     if pull
       puts pull[:title] + "\n\n"
       puts pull[:body]
