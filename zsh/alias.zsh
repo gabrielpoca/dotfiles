84feb3839289c4cf4a98fd3446b5a79b37e7c101
Gabriel Poça <gabrielpoca@gmail.com>
2018-08-31 19:02:13 +0100

    add a bunch of cool aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,1 +1,8 @@
 alias be="bundle exec"
+alias cat="bat"
+alias du="ncdu --color dark -rr -x --exclude .git --exclude node_modules"
+alias help='tldr'
+alias t='terraform'
+alias d='docker'
+alias p='project'
+alias bs='bin/server'
