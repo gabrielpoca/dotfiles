75288cdf43dc8ceebcaacc71edcd44e5f29480dc
Gabriel Poça <gabrielpoca@gmail.com>
2019-06-22 10:54:41 +0100

    [gh] refactor and write tests

--- packages/gh/src/main.rs
+++ packages/gh/src/main.rs
@@ -1,81 +1,53 @@
-use std::env;
-use std::process::Command;
 use clap::{App, SubCommand};
-use std::fs;
 use github_rs::client::{Executor, Github};
 use serde_json::Value;
-use git2::{Repository, Error};
-
-struct Remote {
-    owner: String,
-    repo: String
-}
+use std::env;
+use std::fs;
+use std::process::Command;
+mod git;
+mod tmux;
 
 struct PullRequest {
     title: String,
     body: String,
-    href: String
-}
-
-fn origin_remote<'a>(path: &'a str) -> Result<Remote, Error> {
-    let repo = Repository::open(path)?;
-
-    if repo.is_bare() {
-        return Err(Error::from_str("cannot report status on bare repository"));
-    }
-
-    let remote = repo.find_remote("origin")?;
-    let name = remote.url().unwrap();
-    let path: Vec<&str> = name.split(":").last().unwrap().split("/").collect();
-
-    return Ok(Remote { owner: path[0].into(), repo: path[1].split(".").nth(0).unwrap().into() });
-}
-
-fn current_branch<'a>(path: &'a str) -> Result<String, Error> {
-    let repo = Repository::open(path)?;
-
-    if repo.is_bare() {
-        return Err(Error::from_str("cannot report status on bare repository"));
-    }
-
-    let head = repo.head()?;
-    let name =  head.shorthand().unwrap();
-
-    return Ok(name.into());
+    href: String,
 }
 
 fn github_token() -> String {
     let netrc = fs::read_to_string("/Users/gabrielpoca/.netrc").unwrap();
     let password_line = netrc.split("\n").nth(2).unwrap();
-    return password_line.split(" ").last().unwrap().into()
+    return password_line.split(" ").last().unwrap().into();
 }
 
 fn pull_requests_for_current_branch() -> Result<PullRequest, String> {
     let path = env::current_dir().unwrap();
     let path_str = path.to_str().unwrap();
-
-    let current_branch = current_branch(path_str).unwrap();
-    let remote = origin_remote(path_str).unwrap();
+    let repo = git::repo(path_str).unwrap();
+    let current_branch = git::current_branch(&repo).unwrap();
+    let remote = git::origin_remote(&repo).unwrap();
 
     let client = Github::new(github_token()).unwrap();
-    let pull_requests_endpoint = format!("repos/{}/{}/pulls?head={}:{}", remote.owner, remote.repo, remote.owner, current_branch);
-
-    let (_headers, _status, json) = client.get()
+    let pull_requests_endpoint = format!(
+        "repos/{}/{}/pulls?head={}:{}",
+        remote.owner, remote.repo, remote.owner, current_branch
+    );
+    let (_headers, _status, json) = client
+        .get()
         .custom_endpoint(&pull_requests_endpoint)
-        .execute::<Value>().unwrap();
-
+        .execute::<Value>()
+        .unwrap();
     let pulls = json.unwrap();
-
-
     if pulls[0] == Value::Null {
-        return Err(format!("Cannot find pull request for branch {}", current_branch));
+        return Err(format!(
+                "Cannot find pull request for branch {}",
+                current_branch
+        ));
     } else {
         let pull_request = PullRequest {
             title: pulls[0]["title"].as_str().unwrap().into(),
             body: pulls[0]["body"].as_str().unwrap().into(),
-            href: pulls[0]["html_url"].as_str().unwrap().into()
+            href: pulls[0]["html_url"].as_str().unwrap().into(),
         };
-
         return Ok(pull_request);
     }
 }
@@ -89,57 +61,8 @@ fn pull_request_description() -> Result<(), String> {
 fn pull_request_open() -> Result<(), String> {
     let pull = pull_requests_for_current_branch()?;
     println!("opening {}", pull.href);
-    let output = Command::new("open")
-        .arg(pull.href)
-        .output()
-        .unwrap();
-
+    let output = Command::new("open").arg(pull.href).output().unwrap();
     assert!(output.status.success());
-    return Ok(());
-}
-
-fn tmux_ci_status() -> Result<(), String> {
-    let output = Command::new("tmux")
-        .arg("display-message")
-        .arg("-p")
-        .arg("-F")
-        .arg("\"#{pane_current_path}\"")
-        .output()
-        .unwrap();
-
-    let stdout = output.stdout;
-
-    unsafe {
-        let path = String::from_utf8_unchecked(stdout).trim().replace("\"", "");
-
-        if path.is_empty() {
-            return Ok(());
-        }
-
-        let output = Command::new("hub")
-            .current_dir(path)
-            .arg("ci-status")
-            .output()
-            .unwrap();
-
-        let stdout = output.stdout;
-        let status = String::from_utf8_unchecked(stdout).trim().replace("\"", "");
-
-        if status.is_empty() {
-            return Ok(());
-        }
-
-        if status == "no status" {
-            return Ok(());
-        }
-
-        if status == "success" {
-            return Ok(());
-        }
-
-        println!("ci status is {}", status);
-    }
-
     return Ok(());
 }
 
@@ -149,36 +72,35 @@ fn main() {
         .author("Gabriel Poca")
         .subcommand(
             SubCommand::with_name("pr-description")
-            .about("If there is a PR from this branch, output its title and body")
-        )
+            .about("If there is a PR from this branch, output its title and body"),
+            )
         .subcommand(
             SubCommand::with_name("pr-open")
-            .about("If there is a PR from this branch, open it in the browser")
-        )
+            .about("If there is a PR from this branch, open it in the browser"),
+            )
         .subcommand(
             SubCommand::with_name("tmux-ci-status")
-            .about("Print the ci status for the current tmux pane")
-        )
+            .about("Print the ci status for the current tmux pane"),
+            )
         .get_matches();
-
 
     match matches.subcommand_name() {
         Some("pr-description") => {
             if let Err(e) = pull_request_description() {
                 println!("{}", e);
             }
-        },
+        }
         Some("pr-open") => {
             if let Err(e) = pull_request_open() {
                 println!("{}", e);
             }
-        },
+        }
         Some("tmux-ci-status") => {
-            if let Err(e) = tmux_ci_status() {
+            if let Err(e) = tmux::ci_status() {
                 println!("{}", e);
             }
-        },
+        }
         Some(&_) => println!("No subcommand was used"),
-        None => println!("No subcommand was used")
+        None => println!("No subcommand was used"),
     }
 }
---
+++ packages/gh/src/tmux.rs
@@ -0,0 +1,46 @@
+use std::process::Command;
+
+pub fn ci_status() -> Result<(), String> {
+    let output = Command::new("tmux")
+        .arg("display-message")
+        .arg("-p")
+        .arg("-F")
+        .arg("\"#{pane_current_path}\"")
+        .output()
+        .unwrap();
+
+    let stdout = output.stdout;
+
+    unsafe {
+        let path = String::from_utf8_unchecked(stdout).trim().replace("\"", "");
+
+        if path.is_empty() {
+            return Ok(());
+        }
+
+        let output = Command::new("hub")
+            .current_dir(path)
+            .arg("ci-status")
+            .output()
+            .unwrap();
+
+        let stdout = output.stdout;
+        let status = String::from_utf8_unchecked(stdout).trim().replace("\"", "");
+
+        if status.is_empty() {
+            return Ok(());
+        }
+
+        if status == "no status" {
+            return Ok(());
+        }
+
+        if status == "success" {
+            return Ok(());
+        }
+
+        println!("ci status is {}", status);
+    }
+
+    return Ok(());
+}
