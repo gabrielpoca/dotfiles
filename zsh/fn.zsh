11b699351a53420eea975ad870d638c1b2503642
Gabriel Poça <gabrielpoca@gmail.com>
2019-08-01 10:30:02 +0100

    fn to clean-up docker when running out of space

--- zsh/fn.zsh
+++ zsh/fn.zsh
@@ -79,3 +79,9 @@ fd() {
                   -o -type d -print 2> /dev/null | fzf +m) &&
   cd "$dir"
 }
+
+# docker cleanup
+dcleanup(){
+    docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
+    docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
+}
