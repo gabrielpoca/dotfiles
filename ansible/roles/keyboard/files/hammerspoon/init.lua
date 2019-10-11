de1b347c1fa7b54960417e696c70394756db33ae
Gabriel Poça <gabrielpoca@gmail.com>
2019-10-11 19:26:28 +0100

    karabiner & hammerspoon & use ansible roles
    * moving more configs to dotfiles.
    * start using ansible roles to keep things more organised.

---
+++ ansible/roles/keyboard/files/hammerspoon/init.lua
@@ -0,0 +1,49 @@
+hs.window.animationDuration = 0
+
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "L", function()
+
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "J", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f,0)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, ",", function()
+  hs.applescript('tell application \"iTerm2\" to activate')
+end)
+
+
+local Caffeine = hs.loadSpoon('Caffeine')
+Caffeine:start();
