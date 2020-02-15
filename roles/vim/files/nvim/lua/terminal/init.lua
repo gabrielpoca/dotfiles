3a601311798434d8a281dfcc4506685def2576a4
Gabriel Poça <gabrielpoca@gmail.com>
2020-02-15 17:27:10 +0000

    Better integration between nvim terminals and tig

--- roles/vim/files/nvim/lua/terminal/init.lua
+++ roles/vim/files/nvim/lua/terminal/init.lua
@@ -24,14 +24,34 @@ function to_floating_window(buf)
   local win = vim.api.nvim_open_win(buf, true, opts)
 end
 
-function Terminal(nr, ...)
+function Terminal(nr, command, ...)
+  if command == nil then
+    command = "/bin/zsh"
+  end
+
   if list_of_terms[nr] then
     to_floating_window(list_of_terms[nr])
   else
     list_of_terms[nr] = vim.api.nvim_create_buf(false, false)
     to_floating_window(list_of_terms[nr])
-    vim.api.nvim_call_function("termopen", {"/bin/zsh"})
+    vim.api.nvim_call_function("termopen", {command})
+    vim.api.nvim_command(":au WinLeave <buffer> :close")
   end
 
   vim.api.nvim_command(":startinsert")
+end
+
+tig_current_nr = 100
+
+function TerminalTig()
+  TerminalTigOpen("tig")
+end
+
+function TerminalTigCurrentFile()
+  TerminalTigOpen("tig " .. vim.api.nvim_call_function("expand", {"%:p"}))
+end
+
+function TerminalTigOpen(command)
+  Terminal(tig_current_nr, command)
+  tig_current_nr = tig_current_nr + 1
 end
--- roles/vim/files/nvim/rc/git.vim
+++ roles/vim/files/nvim/rc/git.vim
@@ -2,14 +2,16 @@
 nmap <Leader>gs mzggjvG$:s/^pick/s<CR>
 
 " get the pull request's description
-nmap <leader>gd :r !git pr-description<CR>
+nmap <Leader>gd :r !git pr-description<CR>
 
 " show current file's history in tig, on a new tmux window
-nmap <silent> <leader>gh :silent execute "!tmux new-window tig " . expand("%:p")<CR>
+nnoremap <Leader>gh :lua TerminalTigCurrentFile()<CR>
+
+"nnoremap <Leader>gg :TigOpenProjectRootDir<CR>
+nmap <silent> <Leader>gg :lua TerminalTig()<CR>
 
 " show commits for every source line
-nnoremap <Leader>gb :Gblame<CR> 
-
+nnoremap <Leader>gb :TigGblame<CR> 
 
 " show octobox notifications
 nnoremap <Leader>gn :CocList octobox<CR>
