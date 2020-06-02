f2e9eb8d413996ac77909952fc2ad6b4ab09c771
Gabriel Poça <gabrielpoca@gmail.com>
2020-06-02 13:39:05 +0100

    change hammerspoon applications shortcuts

--- roles/keyboard/files/hammerspoon/applications.lua
+++ roles/keyboard/files/hammerspoon/applications.lua
@@ -1,17 +1,17 @@
 globals = require "globals"
 
 -- open chrome
-hs.hotkey.bind(globals.shift_hyper, "H", function()
+hs.hotkey.bind(globals.shift_hyper, "J", function()
   hs.application.launchOrFocus("Google Chrome")
 end)
 
 -- open iTerm
-hs.hotkey.bind(globals.shift_hyper, "J", function()
+hs.hotkey.bind(globals.shift_hyper, "K", function()
   hs.application.launchOrFocus("iTerm")
 end)
 
 -- open slack
-hs.hotkey.bind(globals.shift_hyper, "K", function()
+hs.hotkey.bind(globals.shift_hyper, "L", function()
   hs.application.launchOrFocus("Slack")
 end)
 
