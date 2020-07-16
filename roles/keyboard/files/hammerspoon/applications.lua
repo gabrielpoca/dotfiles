4dd11d1929552a39e081364e146f17731d7f6484
Gabriel Poça <gabrielpoca@gmail.com>
2020-07-16 14:30:16 +0100

    Add shortcut for NoteDown

--- roles/keyboard/files/hammerspoon/applications.lua
+++ roles/keyboard/files/hammerspoon/applications.lua
@@ -10,6 +10,11 @@ hs.hotkey.bind(globals.shift_hyper, "K", function()
   hs.application.launchOrFocus("iTerm")
 end)
 
+-- open notedown
+hs.hotkey.bind(globals.shift_hyper, ";", function()
+  hs.application.launchOrFocus("NoteDown")
+end)
+
 -- open slack
 hs.hotkey.bind(globals.shift_hyper, "L", function()
   hs.application.launchOrFocus("Slack")
