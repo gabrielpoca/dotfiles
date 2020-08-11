7792b641e0f0224a27ba4ec6a492929da72bd562
Gabriel Poça <gabrielpoca@gmail.com>
2020-08-11 11:07:25 +0100

    [vim] floating windows have borders

--- roles/vim/files/nvim/lua/floating.lua
+++ roles/vim/files/nvim/lua/floating.lua
@@ -1,3 +1,5 @@
+local api = vim.api
+
 function new_buffer(buf)
   local width = vim.api.nvim_get_option("columns")
   local height = vim.api.nvim_get_option("lines")
@@ -19,7 +21,32 @@ function new_buffer(buf)
     col = math.ceil((width - win_width) / 2)
   }
 
+  local border_opts = {
+    style = "minimal",
+    relative = "editor",
+    row = opts.row - 1,
+    col = opts.col - 1,
+    width = opts.width + 2,
+    height = opts.height + 2,
+  }
+
+  local border_lines = {'╭' .. string.rep('─', opts.width) .. '╮'}
+  local middle_line = '│' .. string.rep(' ', opts.width) .. '│'
+  for i = 1, opts.height do
+    table.insert(border_lines, middle_line)
+  end
+  table.insert(border_lines, '╰' .. string.rep('─', opts.width) .. '╯')
+
+  local border_buffer = api.nvim_create_buf(false, true)
+  api.nvim_buf_set_lines(border_buffer, 0, -1, true, border_lines)
+  local border_window = api.nvim_open_win(border_buffer, true, border_opts)
+  vim.cmd 'set winhl=Normal:Floating'
+
   local win = vim.api.nvim_open_win(buf, true, opts)
+
+  -- use autocommand to ensure that the border_buffer closes at the same time as the main buffer
+  local cmd = [[autocmd WinLeave <buffer> silent! execute 'silent bdelete %s']]
+  vim.cmd(cmd:format(border_buffer))
 end
 
 return {
