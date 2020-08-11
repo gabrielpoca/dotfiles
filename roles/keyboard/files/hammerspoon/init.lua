781451b2ac77f5ca06c6a434c37093b774023543
Gabriel Poça <gabrielpoca@gmail.com>
2020-08-11 11:17:40 +0100

    enable/disable bluethooth when the system sleeps/wakes

--- roles/keyboard/files/hammerspoon/init.lua
+++ roles/keyboard/files/hammerspoon/init.lua
@@ -1,6 +1,7 @@
 require "window_placement"
 require "applications"
 require "search"
+require "string"
 
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
@@ -12,13 +13,30 @@ Caffeine:setDisplay(true)
 local HeadphoneAutoPause = hs.loadSpoon('HeadphoneAutoPause')
 HeadphoneAutoPause:start()
 
+function checkBluetoothResult(rc, stderr, stderr)
+    if rc ~= 0 then
+        print(string.format("Unexpected result executing `blueutil`: rc=%d stderr=%s stdout=%s", rc, stderr, stdout))
+    end
+end
+
+function bluetooth(power)
+    print("Setting bluetooth to " .. power)
+    local t = hs.task.new("/usr/local/bin/blueutil", checkBluetoothResult, {"--power", power})
+    t:start()
+end
+
 --https://spinscale.de/posts/2016-11-08-creating-a-productive-osx-environment-hammerspoon.html
-function muteOnWake(eventType)
+function handleCaffeinateEvents(eventType)
   if (eventType == hs.caffeinate.watcher.systemDidWake) then
+    --mute
     local output = hs.audiodevice.defaultOutputDevice()
     output:setMuted(true)
+  elseif eventType == hs.caffeinate.watcher.systemWillSleep then
+    bluetooth("off")
+  elseif eventType == hs.caffeinate.watcher.screensDidWake then
+    bluetooth("on")
   end
 end
 
-caffeinateWatcher = hs.caffeinate.watcher.new(muteOnWake)
+caffeinateWatcher = hs.caffeinate.watcher.new(handleCaffeinateEvents)
 caffeinateWatcher:start()
