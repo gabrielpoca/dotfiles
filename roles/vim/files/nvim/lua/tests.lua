4064cb65d6af2f701418f3a52ff2a28b9825fdb2
Gabriel Poça <gabrielpoca@gmail.com>
2020-10-01 09:57:30 +0100

    [vim] migrate testing keymaps to Lua

--- roles/vim/files/nvim/lua/tests.lua
+++ roles/vim/files/nvim/lua/tests.lua
@@ -1,4 +1,9 @@
-vim.api.nvim_set_keymap('n', "<leader>ra", ":TestSuite<cr>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', "<leader>rt", ":TestFile<cr>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', "<leader>rr", ":TestNearest<cr>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', "<leader>rl", ":TestLast<cr>", { silent = true, noremap = true })
+local api = vim.api
+
+api.nvim_set_keymap('n', "<leader>ra", ":TestSuite<cr>", { silent = true, noremap = true })
+api.nvim_set_keymap('n', "<leader>rt", ":TestFile<cr>", { silent = true, noremap = true })
+api.nvim_set_keymap('n', "<leader>rr", ":TestNearest<cr>", { silent = true, noremap = true })
+api.nvim_set_keymap('n', "<leader>rl", ":TestLast<cr>", { silent = true, noremap = true })
+api.nvim_set_keymap('n', "<leader>rh", ":Ttoggle<cr>", { silent = true, noremap = true })
+api.nvim_set_keymap('n', "<leader>rd", ":Tclear<cr>", { silent = true, noremap = true })
+api.nvim_set_keymap('n', "<leader>rk", ":Tkill<cr>", { silent = true, noremap = true })
--- roles/vim/files/nvim/rc/terminal.vim
+++ roles/vim/files/nvim/rc/terminal.vim
@@ -1,5 +1,3 @@
-lua require("terminal")
-
 " exit terminal mode
 tnoremap <C-e> <C-\><C-n>
 
@@ -12,21 +10,5 @@ let g:neoterm_automap_keys = 'tt'
 let g:neoterm_fixedsize = 1
 let g:neoterm_autoscroll = 1
 
-" toggle terminal split
-nnoremap <silent> <leader>th :Ttoggle<cr>
-" clear terminal
-nnoremap <silent> <leader>tl :Tclear<cr>
-" kill job running in terminal
-nnoremap <silent> <leader>tk :Tkill<cr>
-
 " vim-test settings
 let test#strategy = "neoterm"
-
-" run tests for whole test suite
-nnoremap <silent> <leader>ra :TestSuite<cr>
-" run tests for current file
-nnoremap <silent> <leader>rt :TestFile<cr>
-" run tests for current line
-nnoremap <silent> <leader>rr :TestNearest<cr>
-" re-run last test
-nnoremap <silent> <leader>rl :TestLast<cr>
