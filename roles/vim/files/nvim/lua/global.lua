7ac79e5ffe6ac1efaf6ddbcf6fb15119d3e2f265
Gabriel Poça <gabrielpoca@gmail.com>
2020-10-14 11:13:28 +0100

    [vim] global lua functions for keybindings and reloading

--- roles/vim/files/nvim/lua/global.lua
+++ roles/vim/files/nvim/lua/global.lua
@@ -1,4 +1,18 @@
+local api = vim.api
+
 function _G.dump(...)
-    local objects = vim.tbl_map(vim.inspect, {...})
-    print(unpack(objects))
+  local objects = vim.tbl_map(vim.inspect, {...})
+  print(unpack(objects))
+end
+
+function _G.set_keymaps(mappings, mode)
+  local mode = mode or 'n'
+
+  for k,v in pairs(mappings) do
+    api.nvim_set_keymap(mode, k, ':'..v..'<cr>', { nowait = true, noremap = true, silent = true })
+  end
+end
+
+function _G.reload_current_file()
+  vim.cmd(":luafile %")
 end
