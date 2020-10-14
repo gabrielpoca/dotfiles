71df5cb74423334ae4e93a54800040b0fb32c322
Gabriel Poça <gabrielpoca@gmail.com>
2020-10-14 11:39:30 +0100

    [vim] update keymaps to run tests

--- roles/vim/files/nvim/lua/tests.lua
+++ roles/vim/files/nvim/lua/tests.lua
@@ -1,9 +1,9 @@
-local api = vim.api
-
-api.nvim_set_keymap('n', "<leader>ra", ":TestSuite<cr>", { silent = true, noremap = true })
-api.nvim_set_keymap('n', "<leader>rt", ":TestFile<cr>", { silent = true, noremap = true })
-api.nvim_set_keymap('n', "<leader>rr", ":TestNearest<cr>", { silent = true, noremap = true })
-api.nvim_set_keymap('n', "<leader>rl", ":TestLast<cr>", { silent = true, noremap = true })
-api.nvim_set_keymap('n', "<leader>rh", ":Ttoggle<cr>", { silent = true, noremap = true })
-api.nvim_set_keymap('n', "<leader>rd", ":Tclear<cr>", { silent = true, noremap = true })
-api.nvim_set_keymap('n', "<leader>rk", ":Tkill<cr>", { silent = true, noremap = true })
+set_keymaps({
+    ["<leader>ra"] = "TestSuite",
+    ["<leader>rt"] = "TestFile",
+    ["<leader>rr"] = "TestNearest",
+    ["<leader>rl"] = "TestLast",
+    ["<leader>rh"] = "Ttoggle",
+    ["<leader>rd"] = "Tclear",
+    ["<leader>rk"] = "Tkill"
+  })
