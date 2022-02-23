7c4392ad06023de7f42b7ed8a78ac944ca0b5b3b
Gabriel Poca <mail@gabrielpoca.com>
2022-02-23 00:38:19 +0000

    Remove pod_connect script

--- roles/shell/files/bin/pod_connect.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-result=`kubectl get pods --all-namespaces | fzf`.split(" ").map(&:strip)
-
-command = "kubectl -n #{result[0]} exec -it #{result[1]} -- bash"
-
-system(command)
-
