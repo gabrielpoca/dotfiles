5b47f39dd7d4c214cbac050a5bdb17e772017920
Gabriel Poca <mail@gabrielpoca.com>
2022-02-23 00:49:01 +0000

    [vim] use gf to jump from the terminal to a file

--- roles/vim/files/nvim/lua/navigation.lua
+++ roles/vim/files/nvim/lua/navigation.lua
@@ -1,5 +1,25 @@
 require'nvim-tree'.setup {}
 
+local M = {}
+
+M.term_jump = function ()
+  local parts = vim.fn.split(vim.fn.expand("<cWORD>"), ":");
+  local f = vim.fn.findfile(parts[1])
+
+  if f ~= "" then
+    vim.cmd("FloatermHide")
+    vim.cmd("edit " .. f)
+
+    if vim.fn.empty(parts[2]) == 0 and vim.fn.empty(parts[3]) == 0 then
+      vim.api.nvim_win_set_cursor(win_id, {tonumber(parts[2]), tonumber(parts[3])})
+    elseif vim.fn.empty(parts[2]) == 0 then
+      vim.api.nvim_win_set_cursor(win_id, {tonumber(parts[2]), 1})
+    end
+  end
+end
+
+vim.cmd("autocmd FileType floaterm nnoremap <silent><buffer> gf :lua require('navigation').term_jump()<CR>")
+
 -- clear search highlights
 vim.api.nvim_set_keymap('n', '<leader><cr>', ':nohlsearch<cr><C-L>', { silent = true, noremap = true })
 
@@ -55,7 +75,4 @@ vim.g.projectionist_heuristics = {
     }
 }
 
-set_keymaps({
-        ['<Leader>a'] = 'A',
-        ['<Leader>v'] = 'AV',
-    })
+return M;
