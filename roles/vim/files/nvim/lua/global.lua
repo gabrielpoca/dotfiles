a1d1d8729efa2fd117d5c932bade44ac570f391c
Gabriel Poca <mail@gabrielpoca.com>
2022-05-25 17:24:34 +0100

    [vim] Update _G.dump to use vim.pretty_print

--- roles/vim/files/nvim/lua/global.lua
+++ roles/vim/files/nvim/lua/global.lua
@@ -1,8 +1,7 @@
 local api = vim.api
 
 function _G.dump(...)
-  local objects = vim.tbl_map(vim.inspect, {...})
-  print(unpack(objects))
+  vim.pretty_print(...)
 end
 
 function _G.set_keymaps(mappings, mode)
