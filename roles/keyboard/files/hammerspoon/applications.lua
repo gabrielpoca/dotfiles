c10164e0ffbc20057be3986ded70a905d5f727f3
Gabriel Poca <mail@gabrielpoca.com>
2024-04-23 18:31:20 +0100

    Update lua formatter

--- roles/keyboard/files/hammerspoon/applications.lua
+++ roles/keyboard/files/hammerspoon/applications.lua
@@ -1,21 +1,31 @@
-local globals = require "globals"
+local globals = require("globals")
 
-local function current_app() return hs.application.frontmostApplication():name() end
+local function current_app()
+  return hs.application.frontmostApplication():name()
+end
 
-hs.hotkey.bind(globals.hyper, "L", function()
-    if current_app() == "Alacritty" then
-        hs.application.launchOrFocus("Brave Browser")
-    else
-        hs.application.launchOrFocus("Alacritty")
-    end
+hs.hotkey.bind({ "cmd" }, ";", function()
+  -- hs.hotkey.bind(globals.hyper, "L", function()
+  if current_app() == "WezTerm" then
+    hs.application.launchOrFocus("Arc")
+  else
+    hs.application.launchOrFocus("WezTerm")
+  end
 end)
 
-hs.hotkey.bind(globals.hyper, "K",
-               function() hs.application.launchOrFocus("Slack") end)
+-- hs.hotkey.bind(globals.hyper, "K", function()
+hs.hotkey.bind({ "cmd" }, "'", function()
+  if current_app() == "Slack" then
+    hs.application.launchOrFocus("Arc")
+  else
+    hs.application.launchOrFocus("Slack")
+  end
+end)
 
-hs.hotkey.bind(globals.hyper, "J",
-               function() hs.application.launchOrFocus("Obsidian") end)
+hs.hotkey.bind(globals.hyper, "J", function()
+  hs.application.launchOrFocus("Obsidian")
+end)
 
-hs.hotkey.bind(globals.hyper, "H",
-               function() hs.application.launchOrFocus("TickTick") end)
-
+hs.hotkey.bind(globals.hyper, "H", function()
+  hs.application.launchOrFocus("TickTick")
+end)
--- roles/keyboard/files/hammerspoon/window_placement.lua
+++ roles/keyboard/files/hammerspoon/window_placement.lua
@@ -1,4 +1,4 @@
-globals = require "globals"
+globals = require("globals")
 
 hs.window.animationDuration = 0
 
@@ -6,86 +6,88 @@ local window_gap = 0
 local last_position
 
 function place_window(x, y, w, h)
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
 
-    f.x = x
-    f.y = y
-    f.w = w
-    f.h = h
+  f.x = x
+  f.y = y
+  f.w = w
+  f.h = h
 
-    win:setFrame(f)
+  win:setFrame(f)
 end
 
 function screen_frame()
-    local win = hs.window.focusedWindow()
-    local screen = win:screen()
-    local frame = screen:frame()
-    local full_frame = screen:fullFrame()
+  local win = hs.window.focusedWindow()
+  local screen = win:screen()
+  local frame = screen:frame()
+  local full_frame = screen:fullFrame()
 
-    return {x = frame.x, y = frame.y, w = frame.w, h = full_frame.h}
+  return { x = frame.x, y = frame.y, w = frame.w, h = full_frame.h }
 end
 
 function place_left_half()
-    local frame = screen_frame()
-    local window_width = frame.w / 2
+  local frame = screen_frame()
+  local window_width = frame.w / 2
 
-    if last_position == 'left' then
-        window_width = frame.w / 5 * 3
-        last_position = 'big_left'
-    else
-        last_position = 'left'
-    end
+  if last_position == "left" then
+    window_width = frame.w / 5 * 3
+    last_position = "big_left"
+  else
+    last_position = "left"
+  end
 
-    place_window(frame.x, 0, window_width, frame.h)
+  place_window(frame.x, 0, window_width, frame.h)
 end
 
 function place_right_half()
-    local frame = screen_frame()
-    local window_width = frame.w / 2
+  local frame = screen_frame()
+  local window_width = frame.w / 2
 
-    if last_position == 'right' then
-        window_width = frame.w / 5 * 3
-        last_position = 'big_right'
-    else
-        last_position = 'right'
-    end
+  if last_position == "right" then
+    window_width = frame.w / 5 * 3
+    last_position = "big_right"
+  else
+    last_position = "right"
+  end
 
-    place_window(frame.w - window_width + frame.x, 0, window_width, frame.h)
+  place_window(frame.w - window_width + frame.x, 0, window_width, frame.h)
 end
 
 function place_full()
-    local frame = screen_frame()
-    local x_offset = 0
-    place_window(frame.x + x_offset, frame.y, frame.w - x_offset, frame.h)
-    last_position = 'full'
+  local frame = screen_frame()
+  local x_offset = 0
+  place_window(frame.x + x_offset, frame.y, frame.w - x_offset, frame.h)
+  last_position = "full"
 end
 
 function place_center()
-    local frame = screen_frame()
-    local width = frame.w / 8 * 7
-    local height = frame.h / 8 * 7
-    place_window(frame.x + (frame.w - width) / 2,
-                 frame.y + (frame.h - height) / 2, width, height)
-    last_position = 'center'
+  local frame = screen_frame()
+  local width = frame.w / 8 * 7
+  local height = frame.h / 8 * 7
+  place_window(frame.x + (frame.w - width) / 2, frame.y + (frame.h - height) / 2, width, height)
+  last_position = "center"
 end
 
 function on_application(name, callback)
-    hs.window.filter.new {name}:setAppFilter(name, {
-        allowTitles = 1,
-        hasTitlebar = true
-    }):subscribe(hs.window.filter.windowCreated, callback)
+  hs.window.filter
+    .new({ name })
+    :setAppFilter(name, {
+      allowTitles = 1,
+      hasTitlebar = true,
+    })
+    :subscribe(hs.window.filter.windowCreated, callback)
 end
 
 hs.hotkey.bind(globals.hyper, "Y", place_left_half)
 hs.hotkey.bind(globals.hyper, "O", place_right_half)
 hs.hotkey.bind(globals.hyper, "U", place_full)
 
-on_application('Alacritty', place_full)
-on_application('Brave Browser', place_full)
-on_application('Calendar', place_full)
-on_application('Figma', place_full)
-on_application('Slack', place_full)
-on_application('Telegram', place_right_half)
-on_application('Todoist', place_right_half)
-on_application('Visual Studio Code', place_full)
+on_application("WezTerm", place_full)
+on_application("Brave Browser", place_full)
+on_application("Calendar", place_full)
+on_application("Figma", place_full)
+on_application("Slack", place_full)
+on_application("Telegram", place_right_half)
+on_application("Todoist", place_right_half)
+on_application("Visual Studio Code", place_full)
