0df42bcec94ad5f69dc75b5d6467b544baee4569
Gabriel Poca <mail@gabrielpoca.com>
2024-07-16 08:53:47 +0100

    [vim] update mappings for telescope

--- roles/vim/files/nvim/lua/plugins/telescope.lua
+++ roles/vim/files/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,35 @@
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
+
+local mm = { -- my mappings
+  ["<CR>"] = function(pb)
+    local picker = action_state.get_current_picker(pb)
+    local multi = picker:get_multi_selection()
+    actions.select_default(pb) -- the normal enter behaviour
+    for _, j in pairs(multi) do
+      if j.path ~= nil then -- is it a file -> open it as well:
+        vim.cmd(string.format("%s %s", "edit", j.path))
+      end
+    end
+  end,
+}
+
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = { "nvim-lua/popup.nvim", "nvim-lua/plenary.nvim" },
   opts = {
     defaults = {
+      mappings = {
+        i = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+          ["<C-c>"] = actions.close,
+          ["<C-w>"] = actions.send_selected_to_qflist + actions.open_qflist,
+          ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+          ["<c-d>"] = actions.delete_buffer,
+        },
+        n = { ["<C-c>"] = actions.close },
+      },
       layout_config = { height = 0.9, width = 0.9 },
       file_ignore_patterns = {
         "artifacts/",
