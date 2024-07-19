3c013a936beb6e402b0a7bc7744aeea388c93b62
Gabriel Poca <mail@gabrielpoca.com>
2024-07-19 20:12:32 +0100

    [vim] change default config for better-escape to disable jk in visual and terminal mode

---
+++ roles/vim/files/nvim/lua/plugins/better-escape.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "max397574/better-escape.nvim",
+    opts = {
+      mappings = {
+        i = {
+          j = {
+            k = "<Esc>",
+            j = "<Esc>",
+          },
+        },
+        c = {
+          j = {
+            k = "<Esc>",
+            j = "<Esc>",
+          },
+        },
+        t = {
+          j = false,
+        },
+        v = {
+          j = {
+            k = false,
+          },
+        },
+        s = {
+          j = {
+            k = "<Esc>",
+          },
+        },
+      },
+    },
+  },
+}
