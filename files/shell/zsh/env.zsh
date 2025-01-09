1fd7f8e294a6afabca62c44bdf1fdeefb77c62b8
Gabriel Poca <mail@gabrielpoca.com>
2025-01-09 08:48:20 +0000

    [shell] don't force EDITOR and VISUAL
    Sometimes I'm not using nvim.

--- files/shell/zsh/env.zsh
+++ files/shell/zsh/env.zsh
@@ -1,10 +1,6 @@
 export HOMEBREW_NO_AUTO_UPDATE=1
-# export KERL_BUILD_DOCS="yes"
 
 if [ -n "$NVIM" ]; then
-    export VISUAL="nvr -cc split --remote-wait +'set bufhidden=wipe'"
-    export EDITOR="nvr -cc split --remote-wait +'set bufhidden=wipe'"
-else
-    export VISUAL="nvim"
-    export EDITOR="nvim"
+  export VISUAL="nvr -cc split --remote-wait +'set bufhidden=wipe'"
+  export EDITOR="nvr -cc split --remote-wait +'set bufhidden=wipe'"
 fi
