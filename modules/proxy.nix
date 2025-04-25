d6f066a5f020f20a4239509702ecf0df57f50c2e
Gabriel Poca <mail@gabrielpoca.com>
2025-04-25 11:44:10 +0100

    [nix] update caddy config

--- modules/proxy.nix
+++ modules/proxy.nix
@@ -34,8 +34,8 @@ in
     services.caddy = {
       enable = true;
       package = pkgs.caddy.withPlugins {
-        plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
-        hash = "sha256-JoujVXRXjKUam1Ej3/zKVvF0nX97dUizmISjy3M3Kr8=";
+        plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
+        hash = "sha256-Nwm4kzAmmu+UZTJB5npWdwfgoj3giHIEWIgDF6ff+dY=";
       };
 
       virtualHosts = lib.mapAttrs' (name: hostCfg:
