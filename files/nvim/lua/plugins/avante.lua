602f7f0cf776e4e29df44198edf830b37bf26d0a
Gabriel Poca <mail@gabrielpoca.com>
2025-07-03 08:46:11 +0100

    [nvim] update ai configs

--- files/nvim/lua/plugins/avante.lua
+++ files/nvim/lua/plugins/avante.lua
@@ -1,183 +1,184 @@
-local prefix = "<Leader>a"
-return {
-  "yetone/avante.nvim",
-  build = vim.fn.has "win32" == 1 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-      or "make",
-  event = "User AstroFile",   -- load on file open because Avante manages it's own bindings
-  cmd = {
-    "AvanteAsk",
-    "AvanteBuild",
-    "AvanteEdit",
-    "AvanteRefresh",
-    "AvanteSwitchProvider",
-    "AvanteShowRepoMap",
-    "AvanteModels",
-    "AvanteChat",
-    "AvanteToggle",
-    "AvanteClear",
-    "AvanteFocus",
-    "AvanteStop",
-  },
-  dependencies = {
-    { "stevearc/dressing.nvim", optional = true },
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    { "AstroNvim/astrocore", opts = function(_, opts) opts.mappings.n[prefix] = { desc = " Avante" } end },
-    {
-      "ravitemer/mcphub.nvim",
-      dependencies = {
-        "nvim-lua/plenary.nvim",   -- Required for Job and HTTP requests
-      },
-      -- comment the following line to ensure hub will be ready at the earliest
-      cmd = "MCPHub",                            -- lazy load by default
-      build = "npm install -g mcp-hub@latest",   -- Installs required mcp-hub npm module
-      -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
-      -- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
-      config = function()
-        require("mcphub").setup {
-          extensions = {
-            avante = {
-              make_slash_commands = true,   -- make /slash commands from MCP server prompts
-            },
-          },
-        }
-      end,
-    },
-  },
-  opts = {
-    -- other config
-    -- The system_prompt type supports both a string and a function that returns a string. Using a function here allows dynamically updating the prompt with mcphub
-    system_prompt = function()
-      local hub = require("mcphub").get_hub_instance()
-      return hub:get_active_servers_prompt()
-    end,
-    -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
-    custom_tools = function()
-      return {
-        require("mcphub.extensions.avante").mcp_tool(),
-      }
-    end,
-    rag_service = {
-      enabled = true,                           -- Enables the RAG service
-      host_mount = vim.fn.getcwd(),             -- Host mount path for the rag service
-      provider = "openai",                      -- The provider to use for RAG service (e.g. openai or ollama)
-      llm_model = "",                           -- The LLM model to use for RAG service
-      embed_model = "",                         -- The embedding model to use for RAG service
-      endpoint = "https://api.openai.com/v1",   -- The API endpoint for RAG service
-    },
-    behaviour = {
-      enable_claude_text_editor_tool_mode = true,
-    },
-    mappings = {
-      ask = prefix .. "<CR>",
-      edit = prefix .. "e",
-      refresh = prefix .. "r",
-      focus = prefix .. "f",
-      select_model = prefix .. "?",
-      stop = prefix .. "S",
-      select_history = prefix .. "h",
-      toggle = {
-        default = prefix .. "t",
-        debug = prefix .. "d",
-        hint = prefix .. "h",
-        suggestion = prefix .. "s",
-        repomap = prefix .. "R",
-      },
-      diff = {
-        next = "]c",
-        prev = "[c",
-      },
-      files = {
-        add_current = prefix .. ".",
-        add_all_buffers = prefix .. "B",
-      },
-    },
-  },
-  specs = {   -- configure optional plugins
-    { "AstroNvim/astroui", opts = { icons = { Avante = "" } } },
-    {
-      "Kaiser-Yang/blink-cmp-avante",
-      lazy = true,
-      specs = {
-        {
-          "Saghen/blink.cmp",
-          optional = true,
-          opts = {
-            sources = {
-              default = { "avante" },
-              providers = {
-                avante = { module = "blink-cmp-avante", name = "Avante" },
-              },
-            },
-          },
-        },
-      },
-    },
-    {
-      -- make sure `Avante` is added as a filetype
-      "MeanderingProgrammer/render-markdown.nvim",
-      optional = true,
-      opts = function(_, opts)
-        if not opts.file_types then opts.file_types = { "markdown" } end
-        opts.file_types = require("astrocore").list_insert_unique(opts.file_types, { "Avante" })
-      end,
-    },
-    {
-      -- make sure `Avante` is added as a filetype
-      "OXY2DEV/markview.nvim",
-      optional = true,
-      opts = function(_, opts)
-        if not opts.filetypes then opts.filetypes = { "markdown", "quarto", "rmd" } end
-        opts.filetypes = require("astrocore").list_insert_unique(opts.filetypes, { "Avante" })
-      end,
-    },
-    {
-      "folke/snacks.nvim",
-      optional = true,
-      specs = {
-        {
-          "yetone/avante.nvim",
-          opts = {
-            selector = {
-              provider = "snacks",
-            },
-          },
-        },
-      },
-    },
-    {
-      "nvim-neo-tree/neo-tree.nvim",
-      optional = true,
-      opts = {
-        filesystem = {
-          commands = {
-            avante_add_files = function(state)
-              local node = state.tree:get_node()
-              local filepath = node:get_id()
-              local relative_path = require("avante.utils").relative_path(filepath)
-
-              local sidebar = require("avante").get()
-
-              local open = sidebar:is_open()
-              -- ensure avante sidebar is open
-              if not open then
-                require("avante.api").ask()
-                sidebar = require("avante").get()
-              end
-
-              sidebar.file_selector:add_selected_file(relative_path)
-
-              -- remove neo tree buffer
-              if not open then sidebar.file_selector:remove_selected_file "neo-tree filesystem [1]" end
-            end,
-          },
-        },
-        window = {
-          mappings = {
-            ["oa"] = "avante_add_files",
-          },
-        },
-      },
-    },
-  },
-}
+return {}
+-- local prefix = "<Leader>a"
+-- return {
+--   "yetone/avante.nvim",
+--   build = vim.fn.has "win32" == 1 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+--       or "make",
+--   event = "User AstroFile",   -- load on file open because Avante manages it's own bindings
+--   cmd = {
+--     "AvanteAsk",
+--     "AvanteBuild",
+--     "AvanteEdit",
+--     "AvanteRefresh",
+--     "AvanteSwitchProvider",
+--     "AvanteShowRepoMap",
+--     "AvanteModels",
+--     "AvanteChat",
+--     "AvanteToggle",
+--     "AvanteClear",
+--     "AvanteFocus",
+--     "AvanteStop",
+--   },
+--   dependencies = {
+--     { "stevearc/dressing.nvim", optional = true },
+--     "nvim-lua/plenary.nvim",
+--     "MunifTanjim/nui.nvim",
+--     { "AstroNvim/astrocore", opts = function(_, opts) opts.mappings.n[prefix] = { desc = " Avante" } end },
+--     {
+--       "ravitemer/mcphub.nvim",
+--       dependencies = {
+--         "nvim-lua/plenary.nvim",   -- Required for Job and HTTP requests
+--       },
+--       -- comment the following line to ensure hub will be ready at the earliest
+--       cmd = "MCPHub",                            -- lazy load by default
+--       build = "npm install -g mcp-hub@latest",   -- Installs required mcp-hub npm module
+--       -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
+--       -- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+--       config = function()
+--         require("mcphub").setup {
+--           extensions = {
+--             avante = {
+--               make_slash_commands = true,   -- make /slash commands from MCP server prompts
+--             },
+--           },
+--         }
+--       end,
+--     },
+--   },
+--   opts = {
+--     -- other config
+--     -- The system_prompt type supports both a string and a function that returns a string. Using a function here allows dynamically updating the prompt with mcphub
+--     system_prompt = function()
+--       local hub = require("mcphub").get_hub_instance()
+--       return hub:get_active_servers_prompt()
+--     end,
+--     -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+--     custom_tools = function()
+--       return {
+--         require("mcphub.extensions.avante").mcp_tool(),
+--       }
+--     end,
+--     rag_service = {
+--       enabled = true,                           -- Enables the RAG service
+--       host_mount = vim.fn.getcwd(),             -- Host mount path for the rag service
+--       provider = "openai",                      -- The provider to use for RAG service (e.g. openai or ollama)
+--       llm_model = "",                           -- The LLM model to use for RAG service
+--       embed_model = "",                         -- The embedding model to use for RAG service
+--       endpoint = "https://api.openai.com/v1",   -- The API endpoint for RAG service
+--     },
+--     behaviour = {
+--       enable_claude_text_editor_tool_mode = true,
+--     },
+--     mappings = {
+--       ask = prefix .. "<CR>",
+--       edit = prefix .. "e",
+--       refresh = prefix .. "r",
+--       focus = prefix .. "f",
+--       select_model = prefix .. "?",
+--       stop = prefix .. "S",
+--       select_history = prefix .. "h",
+--       toggle = {
+--         default = prefix .. "t",
+--         debug = prefix .. "d",
+--         hint = prefix .. "h",
+--         suggestion = prefix .. "s",
+--         repomap = prefix .. "R",
+--       },
+--       diff = {
+--         next = "]c",
+--         prev = "[c",
+--       },
+--       files = {
+--         add_current = prefix .. ".",
+--         add_all_buffers = prefix .. "B",
+--       },
+--     },
+--   },
+--   specs = {   -- configure optional plugins
+--     { "AstroNvim/astroui", opts = { icons = { Avante = "" } } },
+--     {
+--       "Kaiser-Yang/blink-cmp-avante",
+--       lazy = true,
+--       specs = {
+--         {
+--           "Saghen/blink.cmp",
+--           optional = true,
+--           opts = {
+--             sources = {
+--               default = { "avante" },
+--               providers = {
+--                 avante = { module = "blink-cmp-avante", name = "Avante" },
+--               },
+--             },
+--           },
+--         },
+--       },
+--     },
+--     {
+--       -- make sure `Avante` is added as a filetype
+--       "MeanderingProgrammer/render-markdown.nvim",
+--       optional = true,
+--       opts = function(_, opts)
+--         if not opts.file_types then opts.file_types = { "markdown" } end
+--         opts.file_types = require("astrocore").list_insert_unique(opts.file_types, { "Avante" })
+--       end,
+--     },
+--     {
+--       -- make sure `Avante` is added as a filetype
+--       "OXY2DEV/markview.nvim",
+--       optional = true,
+--       opts = function(_, opts)
+--         if not opts.filetypes then opts.filetypes = { "markdown", "quarto", "rmd" } end
+--         opts.filetypes = require("astrocore").list_insert_unique(opts.filetypes, { "Avante" })
+--       end,
+--     },
+--     {
+--       "folke/snacks.nvim",
+--       optional = true,
+--       specs = {
+--         {
+--           "yetone/avante.nvim",
+--           opts = {
+--             selector = {
+--               provider = "snacks",
+--             },
+--           },
+--         },
+--       },
+--     },
+--     {
+--       "nvim-neo-tree/neo-tree.nvim",
+--       optional = true,
+--       opts = {
+--         filesystem = {
+--           commands = {
+--             avante_add_files = function(state)
+--               local node = state.tree:get_node()
+--               local filepath = node:get_id()
+--               local relative_path = require("avante.utils").relative_path(filepath)
+--
+--               local sidebar = require("avante").get()
+--
+--               local open = sidebar:is_open()
+--               -- ensure avante sidebar is open
+--               if not open then
+--                 require("avante.api").ask()
+--                 sidebar = require("avante").get()
+--               end
+--
+--               sidebar.file_selector:add_selected_file(relative_path)
+--
+--               -- remove neo tree buffer
+--               if not open then sidebar.file_selector:remove_selected_file "neo-tree filesystem [1]" end
+--             end,
+--           },
+--         },
+--         window = {
+--           mappings = {
+--             ["oa"] = "avante_add_files",
+--           },
+--         },
+--       },
+--     },
+--   },
+-- }
