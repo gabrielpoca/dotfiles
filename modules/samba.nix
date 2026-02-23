eabee0d40361be0fa2a15ed3400e2b70c7ccd5c2
Gabriel Poca <mail@gabrielpoca.com>
2026-02-23 22:18:55 +0000

    [nix] fix samba share configuration format

--- modules/samba.nix
+++ modules/samba.nix
@@ -21,39 +21,39 @@ in
     services.samba = {
       enable = true;
 
-      settings = {
-        global = {
-          "create mask" = "0775";
-          "directory mask" = "0775";
-          "server role" = "standalone";
-          "guest account" = "nobody";
-          "map to guest" = "bad user";
-          "security" = "auto";
-          "socket options" = "TCP_NODELAY IPTOS_LOWDELAY SO_RCVBUF=131072 SO_SNDBUF=131072";
-          "min receivefile size" = "16384";
-          "use sendfile" = "yes";
-          "aio read size" = "16384";
-          "aio write size" = "16384";
-          "read raw" = "yes";
-          "write raw" = "yes";
-          "getwd cache" = "yes";
-          "unix extensions" = "no";
-          "follow symlinks" = "yes";
-        };
-      };
-
-      shares = builtins.listToAttrs (map
-        (folder: {
-          name = baseNameOf folder;
-          value = {
-            path = folder;
-            public = true;
-            writable = true;
-            "guest ok" = "yes";
-            "guest only" = "yes";
+      settings =
+        {
+          global = {
+            "create mask" = "0775";
+            "directory mask" = "0775";
+            "server role" = "standalone";
+            "guest account" = "nobody";
+            "map to guest" = "bad user";
+            "security" = "auto";
+            "socket options" = "TCP_NODELAY IPTOS_LOWDELAY SO_RCVBUF=131072 SO_SNDBUF=131072";
+            "min receivefile size" = "16384";
+            "use sendfile" = "yes";
+            "aio read size" = "16384";
+            "aio write size" = "16384";
+            "read raw" = "yes";
+            "write raw" = "yes";
+            "getwd cache" = "yes";
+            "unix extensions" = "no";
+            "follow symlinks" = "yes";
           };
-        })
-        cfg.folders);
+        }
+        // builtins.listToAttrs (map
+          (folder: {
+            name = baseNameOf folder;
+            value = {
+              path = folder;
+              public = true;
+              writable = true;
+              "guest ok" = "yes";
+              "guest only" = "yes";
+            };
+          })
+          cfg.folders);
     };
   };
 }
