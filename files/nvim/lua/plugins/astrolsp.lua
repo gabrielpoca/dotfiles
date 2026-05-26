46bbe264837d55bb4e8c4ce8c1a2b3a81a21ce8e
Gabriel Poca <mail@gabrielpoca.com>
2026-05-26 09:06:08 +0100

    nvim: update plugins and elixir ls config

--- files/nvim/lua/plugins/astrolsp.lua
+++ files/nvim/lua/plugins/astrolsp.lua
@@ -15,24 +15,11 @@ return {
     servers = {
       "elixirls",
     },
-    -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
-    },
-    -- customize how language servers are attached
-    handlers = {
-      -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
-      -- function(server, opts) require("lspconfig")[server].setup(opts) end
-
-      -- the key is the server that is being setup with `lspconfig`
-      -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
-      -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
-      elixirls = function(_, opts)
-        require("lspconfig")["elixirls"].setup(vim.tbl_extend("keep", {
-          cmd = { vim.fn.expand "~/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" },
-        }, opts))
-      end,
+      elixirls = {
+        cmd = { vim.fn.expand "~/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" },
+      },
     },
   },
 }
